b4fc4af9d747f5c98a6676e12c35c528824398e5
Marlon Pugss <92606374+marlon-pugss@users.noreply.github.com>
2024-10-01 17:18:01 -0300

    requirements

--- src/analise.py
+++ src/analise.py
@@ -1,43 +1,45 @@
 import uuid
-from helper import extract_data_analysis, get_pdf_paths, read_uploaded_file
+import streamlit as st
+from helper import extract_data_analysis, read_uploaded_file
 from database import AnalyzeDatabase
 from ai import GroqClient
 from models.resum import Resum
 from models.file import File
 
+# Inicializando o banco de dados e o cliente AI
 database = AnalyzeDatabase()
 ai = GroqClient()
 job = database.get_job_by_name('Vaga de Gestor Comercial de B2B')
 
-cv_paths = get_pdf_paths(directory='/Users/fluencyacademy/projeto-pessoal/src/drive/curriculos')
+# Permitir o upload de múltiplos arquivos PDF
+uploaded_files = st.file_uploader("Carregue seus currículos", type=["pdf"], accept_multiple_files=True)
 
-for path in cv_paths:
-    content = read_uploaded_file(path)
-    print(content)
-    resum = ai.resume_cv(content)
-    print(resum)
-    opinion = ai.generate_opinion(content, job)
-    print(opinion)
-    score = ai.generate_score(content, job)
-    print(score)
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        content = read_uploaded_file(uploaded_file)
+        st.write(content)  # Use st.write ou st.text para exibir conteúdo em vez de print
+        resum = ai.resume_cv(content)
+        st.write(resum)
+        opinion = ai.generate_opinion(content, job)
+        st.write(opinion)
+        score = ai.generate_score(content, job)
+        st.write(score)
 
-    resum_schema = Resum(
-        id=str(uuid.uuid4()),
-        job_id=job.get('id'),
-        content=resum,
-        file=str(path),
-        opinion=opinion
-    )
+        resum_schema = Resum(
+            id=str(uuid.uuid4()),
+            job_id=job.get('id'),
+            content=resum,
+            file=uploaded_file.name,
+            opinion=opinion
+        )
 
-    file_schema = File(
-        file_id=str(uuid.uuid4()),
-        job_id=job.get('id')
-    )
+        file_schema = File(
+            file_id=str(uuid.uuid4()),
+            job_id=job.get('id')
+        )
 
-    analyzis_schema = extract_data_analysis(resum,job.get('id'), resum_schema.id, score)
+        analyzis_schema = extract_data_analysis(resum, job.get('id'), resum_schema.id, score)
 
-    database.resums.insert(resum_schema.model_dump())
-    database.analysis.insert(analyzis_schema.model_dump())
-    database.files.insert(file_schema.model_dump())
-
-
+        database.resums.insert(resum_schema.model_dump())
+        database.analysis.insert(analyzis_schema.model_dump())
+        database.files.insert(file_schema.model_dump())
