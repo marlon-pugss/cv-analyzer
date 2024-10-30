f8f4c07cd78096931f3d70f06990f4fd940647b4
Marlon Pugss <92606374+marlon-pugss@users.noreply.github.com>
2024-10-30 20:34:14 -0300

    validate_job_description

--- src/app.py
+++ src/app.py
@@ -19,7 +19,9 @@ job_description = st.text_area("Descrição da Vaga", placeholder="Insira aqui a
 # Verifica se currículos foram carregados e se a descrição da vaga foi fornecida
 if uploaded_files and job_description:
     # Valida a descrição da vaga
+    st.write("Validando a descrição da vaga...")
     job_validation = ai.validate_job_description(job_description)
+
     
     if job_validation == "Descrição de vaga inválida":
         st.warning("⚠️ Descrição de vaga inválida. Por favor, insira uma descrição válida.")
